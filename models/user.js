f63657bb2ef41ec72052110172e96ea14b6db538
Andrew Chang <andychang.92@gmail.com>
2019-12-07 12:16:31 -0500

    created basic user table

--- models/user.js
+++ models/user.js
@@ -5,7 +5,6 @@ module.exports = (sequelize, DataTypes) => {
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    expertise: DataTypes.STRING
   }, {});
   User.associate = function(models) {
     // associations can be defined here
