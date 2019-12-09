9065d8b055482e1ce1702f0c455b138d9ddfd839
Andrew Chang <andychang.92@gmail.com>
2019-12-09 10:57:14 -0500

    created signup controller

--- controllers/index.js
+++ controllers/index.js
@@ -7,8 +7,25 @@ const TOKEN_KEY = "ahj"
 
 const signUp = async (req,res) => {
     try{
-        const {}
+        const {firstName, lastName, email, password} =req.body
+        const password_digest = await bcrypt.hash(password, SALT_ROUNDS)
+        const user = await user.create({
+            firstName,
+            lastName,
+            email,
+            password_digest
+        })
+        const payload = {
+            id: user.id,
+            firstName: user.firstName,
+            lastName:user.lastName,
+            email: user.email
+        }
+
+        const token = jwt.sign(payload, TOKEN_KEY)
+        return res.status(201).json({user, token})
     }catch(error){
-        
+        console.log('You made it to the sign-up controller, but there was an error')
+        return res.status(400).json({error: error.message})
     }
 }
