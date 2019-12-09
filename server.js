c7b8798a40973c1b522c9410a7d511c8b4648b91
Andrew Chang <andychang.92@gmail.com>
2019-12-09 09:45:48 -0500

    server now properly connects

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ const app = require('./app.js');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("This is root!");
+});
+
+module.exports = app;
