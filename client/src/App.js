af527f0772acac00881c60d22b1f15351490144e
Andrew Chang <andychang.92@gmail.com>
2019-12-09 14:20:51 -0500

    added nav and routes for FE sign in/up

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import {Route, withRouter} from 'react-router-dom'
 import Home from './components/routes/Home'
-// import SignIn from './components/routes/SignIn';
+import SignIn from './components/routes/SignIn';
+import SignUp from './components/routes/SignUp'
 import TeamRoster from './components/routes/TeamRoster';
 import Header from './components/shared/Header'
 import AvailableCoders from './components/routes/AvailableCoders';
+import { sign } from 'crypto';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
      <Route exact path='/' component={Home} />
      <Route exact path='/teamroster' component={TeamRoster} />
      <Route exact path='/availablecoders' component={AvailableCoders} />
+     <Route exact path='/sign-in' component={SignIn}/>
+     <Route exact path='/sign-up' component={SignUp}/>
     </div> 
   );
 }
--- client/src/components/shared/Nav.jsx
+++ client/src/components/shared/Nav.jsx
@@ -7,6 +7,8 @@ const Nav = () => {
             <NavLink to= '/'>Home</NavLink>
             <NavLink to ='/teamroster'>Team Roster</NavLink>
             <NavLink to='/availablecoders'>Available Coders</NavLink>
+            <NavLink to='/sign-in'>Log in</NavLink>
+            <NavLink to='/sign-up'>Create Account</NavLink>
             
         </nav>
     )
