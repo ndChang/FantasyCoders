569584f36bdaff6b1cb794baff9efc58aa03a1d8
Andrew Chang <andychang.92@gmail.com>
2019-12-09 11:45:21 -0500

    added sign-in router

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = Router()
 
 router.get('/', (req,res) => res.send('This is root file for fantasy coder!'))
 router.post('/sign-up', controllers.signUp)
+router.post('/sign-in', controllers.signIn)
 
 module.exports = router
 
