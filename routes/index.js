a6eb671c549dfd33f9961b178da1a19e3af9184d
Jowel <rjowel@gmail.com>
2019-12-09 17:04:15 -0500

    katelyn committing

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const {Router } = require('express')
 const controllers =require('../controllers')
 const router = Router()
 
+
 router.get('/', (req,res) => res.send('This is root file for fantasy coder!'))
 router.post('/sign-up', controllers.signUp)
 router.post('/sign-in', controllers.signIn)
