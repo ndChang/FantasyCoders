5dbc2356bdb1d1084b278674ab9943aee0f2523d
Andrew Chang <andychang.92@gmail.com>
2019-12-10 10:46:20 -0500

    sign up skeleton

--- client/src/components/routes/SignIn.jsx
+++ client/src/components/routes/SignIn.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 class SignIn extends React.Component {
     constructor() {
         super()
-        this.state = ''
+        this.state = {
+            email: '',
+            password: '',
+            isError: false,
+            errorMsg: ''
+        }
+    }
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value,
+            isError:false,
+            errorMsg: ''
+        })
+    }
+
+    onSignIn = event => {
+        event.preventDefault()
+        const { history, setUser} = this.props
+        signInUser(this.state).then(res => setUser(res.user))
     }
 
     render() {
