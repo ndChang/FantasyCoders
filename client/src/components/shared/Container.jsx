d8d9787a7c6becdd26b797a7f06ea6662109711c
Hadeeqa Saman <hadeeqasaman95@gmail.com>
2019-12-10 11:08:32 -0500

    rename folder

--- client/src/components/shared/Container.jsx
+++ client/src/components/shared/Container.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Routes from '../routes'
+import Routes from '../screens'
 import Nav from '../shared/Nav'
 
 export default class Container extends React.Component {
