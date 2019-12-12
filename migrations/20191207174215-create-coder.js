c96b326fbbffdd88ba5e6c2dd8ae504c5d0f0473
Jowel <rjowel@gmail.com>
2019-12-12 11:21:44 -0500

    trouble shooting

--- migrations/20191207174215-create-coder.js
+++ migrations/20191207174215-create-coder.js
@@ -18,7 +18,13 @@ module.exports = {
         type: Sequelize.STRING
       },
       userId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'User',
+          key: 'id',
+          as: 'userId'
+        }
       },
       createdAt: {
         allowNull: false,
